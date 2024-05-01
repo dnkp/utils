15e5dd4ee21b6e514ceda9e51cbb3fba1b5f3ad2
Dejan Petrovic <dejan.petrovic@nortal.com>
2024-05-02 00:50:36 +0200

    moved function generate_csr_and_key

--- csrgen.sh
+++ csrgen.sh
@@ -10,6 +10,7 @@
 
 # Initialize variables with default values
 inventory_file="hosts.txt"
+hostname=""
 
 # Define variables for subject parameters
 country="SX"
@@ -18,23 +19,44 @@ locality="XXXXX"
 organization="XXXXXX"
 organizational_unit="XX"
 
-
 # Function to display usage/help message
 display_help() {
     echo "Usage: $0 [OPTIONS]"
     echo "Options:"
     echo "  -i, --inventory <inventory_file>  Specify the file with domain names (default: hosts.txt)"
-    echo "  -h, --help                            Display this help message"
-    echo "This script reads a list of hostnames from the file (one hostname per line) and generates a CSR and a private key for each hostname."
+    echo "  -h, --help                        Display this help message"
+    echo "  --hostname <hostname>             Specify the hostname for which CSR and private key will be generated"
+    echo "This script generates a CSR and a private key for the specified hostname."
     exit 0
 }
+
+
+# Function to generate CSR and Private Key
+generate_csr_and_key() {
+    local namehost="$1"
+
+    # Cover the case when provided hostname is longer than 64 characters
+    local cn_namehost="$namehost"
+    if [[ ${#namehost} -gt 64 ]]; then
+        echo "Hostname '$namehost' is longer than maximum allowed 64 characters. Hostname stripped to '${namehost%%.*}'."
+        cn_namehost="${namehost%%.*}"
+    fi
+
+    openssl req -new -newkey rsa:2048 -nodes -keyout "${namehost%%.*}.key" -out "${namehost%%.*}.csr" -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizational_unit/CN=${cn_namehost}" -addext "subjectAltName = DNS:$namehost" -addext "keyUsage = digitalSignature, dataEncipherment" -addext "extendedKeyUsage = serverAuth, clientAuth" 2>/dev/null
+
+    if [[ $? -eq 0 ]]; then
+        echo "Generated CSR and private key for ${cn_namehost}."
+    else
+        echo "Failed to generate CSR and private key for ${cn_namehost}."
+    fi
+}
+
 
 # Check if OpenSSL is installed
 if ! command -v openssl &>/dev/null; then
     echo "Error: OpenSSL is not installed. Please install it before running this script."
     exit 1
 fi
-
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -43,9 +65,13 @@ while [[ $# -gt 0 ]]; do
             shift
             inventory_file="$1"
             ;;
-	-h|--help)
-	    display_help
-	    ;;
+        -h|--help)
+            display_help
+            ;;
+        --hostname)
+            shift
+            hostname="$1"
+            ;;
         *)
             echo "Invalid argument: $1"
             exit 1
@@ -54,41 +80,28 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-# Check if provided inventory file exists
-if [[ ! -f "$inventory_file" ]]; then
-    echo "Inventory file '$inventory_file' does not exist in the directory."
-    display_help
-    exit 1
+# Check if hostname is provided
+if [[ -z "$hostname" ]]; then
+    # Check if provided inventory file exists
+    if [[ ! -f "$inventory_file" ]]; then
+        echo "Inventory file '$inventory_file' does not exist in the directory."
+        display_help
+        exit 1
+    fi
+
+    # Create array from the provided names
+    hosts_array=()
+    while read -r host; do
+        hosts_array+=("$host")
+    done < "$inventory_file"
+
+    # Iterate through the list of hostnames and generate CSR and private key
+    for namehost in "${hosts_array[@]}"; do
+        generate_csr_and_key "$namehost"
+    done
+else
+    generate_csr_and_key "$hostname"
 fi
 
-# Create array from the provided names
-while read -r host; do
-    hosts_array+=("$host")
-done < "$inventory_file"
-
-
-# Iterate trough the list of hostnames and generate csr and private key 
-for namehost in "${hosts_array[@]}"
-do
-    # Cover the case when provided hostname is longer than 64 characters
-    cn_namehost=$namehost
-
-
-    # Check if hostname is longer than maximum allowed 64 characters, if longer strip it.
-    if [[ ${#namehost} -gt 64 ]] ; then
-    echo "Hostname '$namehost' is longer than maximum allowed 64 characters. Hostname stripped to '${namehost%%.*}'."
-    cn_namehost=${namehost%%.*}
-    fi
-
-    openssl req -new -newkey rsa:2048 -nodes -keyout ${namehost%%.*}.key -out ${namehost%%.*}.csr -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizational_unit/CN=${cn_namehost}" -addext "subjectAltName = DNS:$namehost" -addext "keyUsage = digitalSignature, dataEncipherment" -addext "extendedKeyUsage = serverAuth, clientAuth" 2>/dev/null
-
-    if [[ $? -eq 0 ]]; then
-        echo "Generated for ${cn_namehost}."
-
-    else
-        echo "Failed to generate for ${cn_namehost}."
-    fi
-
-done
-
 exit 0
+
